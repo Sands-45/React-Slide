2d38f6b58c03e6c07054deccae7f825819116e35
Sands-45 <sandsqa@hotmail.com>
2021-09-23 00:03:14 +0200

    initial commit

---
+++ src/components/main.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+
+const Main = ({ tasks }) => {
+  let initialCount = 0;
+  const [count, setCount] = useState(initialCount);
+  const list = tasks.map((item, index) => {
+    if (index === count) {
+      return (
+        <>
+          <h1 className="text-[3em] text-[#fff] font-bold">{item.title}</h1>
+          <p className="text-[#fff]">{item.text}</p>
+        </>
+      );
+    }
+  });
+
+  function disablePre() {
+    if (count === 0) {
+      return true;
+    }
+  }
+
+  function disableNext() {
+    if (count === tasks.length - 1) {
+      return true;
+    }
+  }
+
+  return (
+    <div className="h-[100vh] bg-[#343da3] flex items-center justify-center select-none">
+      <div className="bg-[#5561E5] p-5 flex items-start justify-center flex-col w-6/12 rounded-lg">
+        {list}
+        <div className="flex space-x-7">
+          <button
+            onClick={() => setCount(initialCount)}
+            className="p-2 mt-4 bg-[#fff] rounded-md disabled:opacity-50"
+            disabled={disablePre()}
+          >
+            Restart
+          </button>
+          <button
+            onClick={() =>
+              setCount((prevCount) =>
+                prevCount < tasks.length - 1 ? prevCount + 1 : (prevCount = 2)
+              )
+            }
+            className="p-2 mt-4 bg-[#fff] rounded-md disabled:opacity-50"
+            disabled={disableNext()}
+          >
+            Next
+          </button>
+          <button
+            onClick={() =>
+              setCount((prevCount) =>
+                prevCount > 0 ? prevCount - 1 : (prevCount = 0)
+              )
+            }
+            className="p-2 mt-4 bg-[#fff] rounded-md disabled:opacity-50"
+            disabled={disablePre()}
+          >
+            Previous
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
